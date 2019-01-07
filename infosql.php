b2395a9a1cccf8862b30c39d80ffca7e41ea66ac
Krez90 <bilel.hamimed@yahoo.fr>
2019-01-07 10:14:28 +0100

    majdulundi

---
+++ infosql.php
@@ -0,0 +1,11 @@
+<?php
+//Renommez ce fichier en info.php, avec vos informations personnelles.
+
+$host = "localhost";
+$dbname = "transfert;charset=utf8";
+$user = "root";
+$pass = "";
+
+$base_url = "http://localhost//";
+
+?>
