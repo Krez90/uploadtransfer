d8886d4b28945a3e13ae431195b929cfb0ee4735
TA-A <thecleaurelie.augustin@gmail.com>
2019-01-14 09:28:18 +0100

    formulaire + upload

--- formulaire.php
+++ formulaire.php
@@ -17,21 +17,51 @@ $message = "L'adresse n'est pas valide <br>";
 if (isset($cheminetnomTemporaire) && isset($_fichier) && isset($_FILES)){
     $cheminetnomTemporaire = $_FILES['fichier']['tmp_name'];
     $_fichier = basename ($_FILES['fichier']['name']);
-    $_FILES['fichier']['name'] = time().$_FILES['fichier']['name'];
     $_fichier = 'fichier_upload/'.$_FILES['fichier']['name'];
+    $moveIsOk = move_uploaded_file($cheminetnomTemporaire, $_fichier);
   }
+
+// Générer le code aléatoire
+$characts = 'abcdefghijklmnopqrstuvwxyz'; 
+$characts .= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';	
+$characts .= '1234567890'; 
+$code_aleatoire = '';
+
+for($i=0;$i < 10;$i++) 
+{ 
+$code_aleatoire .= $characts[ rand() % strlen($characts) ]; 
+}
+// Fin code aléatoire
+
+// FICHIER ZIP
+
+$filename = "fichier_upload/UploadIt".time().".zip";
+
+if(isset($zip)){
+if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+    exit("Impossible d'ouvrir le fichier <$filename>\n");
+}
+if(isset($_fichier)){
+$zip->addFile($_fichier);
+}
+
+// echo "Nombre de fichiers : " . $zip->numFiles . "\n";
+// echo "Statut :" . $zip->status . "\n";
+$zip->close();
+}
+// FIN FICHIER ZIP
 
 include('infosql.php');
 $dbh = new PDO('mysql:host='. $host .';dbname='. $dbname, $user, $pass); 
 
-function addFichier($filename,$code_aleatoire){
+function addFichier($filename, $code_aleatoire){
     global $dbh;
 
 $add_fichier = $dbh->prepare('INSERT INTO upload (Url, code, date) VALUES (?,?, NOW());');
-$add_fichier->execute([$filename,$code_aleatoire]);
+$add_fichier->execute([$filename, $code_aleatoire]);
 
 }
-if (isset($filename)){
+if(isset($filename)){
 addFichier($filename, $code_aleatoire);
 }
 
